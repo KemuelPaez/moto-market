3fc8de06be69e08895a6aa20b90274accfd6732b
Kemuel Paez <kemkylepaez@gmail.com>
2025-09-22 01:32:44 +0800

    changed image placeholders

--- src/data/products.js
+++ src/data/products.js
@@ -4,35 +4,35 @@ const products = [
     name: 'CBR 650R',
     brand: 'Honda',
     price: 9250,
-    image: 'https://images.unsplash.com/photo-1515859005217-3d1f1d0b0b9b?w=1200&q=60'
+    image: 'https://images.unsplash.com/photo-1653064098555-ae5a6e81ec05?q=80&w=1151&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'moto-2',
     name: 'Ninja ZX-6R',
     brand: 'Kawasaki',
     price: 10500,
-    image: 'https://images.unsplash.com/photo-1511381939415-4d2a4a9f03a7?w=1200&q=60'
+    image: 'https://images.unsplash.com/photo-1682980798344-6feac9455e66?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'moto-3',
     name: 'YZF-R3',
     brand: 'Yamaha',
     price: 5200,
-    image: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1200&q=60'
+    image: 'https://images.unsplash.com/photo-1643824762078-d7307c468ee6?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'moto-4',
     name: 'Street Triple R',
     brand: 'Triumph',
     price: 11500,
-    image: 'https://images.unsplash.com/photo-1514474959185-4f77f8d2e5d5?w=1200&q=60'
+    image: 'https://images.unsplash.com/photo-1508431432310-7fec11a74c59?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'moto-5',
     name: 'Bonneville T120',
     brand: 'Triumph',
     price: 12300,
-    image: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1200&q=60'
+    image: 'https://images.unsplash.com/photo-1723720222378-0c7ab126b200?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   }
 ]
 
